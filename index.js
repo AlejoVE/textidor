36162798eb74db98f781115efaf0d2fe14d1573e
Luis Alejandro Montilla <luis_alejandro_499@hotmailcom>
2020-05-10 22:28:56 +0200

    routes completed

--- index.js
+++ index.js
@@ -1,66 +1,66 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const cors = require('cors');
-const fs = require('fs');
-const path = require('path');
-const config = require('./config');
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const cors = require("cors");
+const fs = require("fs");
+const path = require("path");
+const config = require("./config");
 
 // - setup -
-const FILES_DIR = __dirname + '/text-files';
+const FILES_DIR = __dirname + "/text-files";
 // create the express app
-_;
+const app = express();
 
 // - use middleware -
 // allow Cross Origin Resource Sharing
 app.use(cors());
 // parse the body
-_;
+app.use(bodyParser.raw({ type: "text/plain" }));
+app.use(bodyParser.json());
 
 // https://github.com/expressjs/morgan#write-logs-to-a-file
 const accessLogStream = fs.createWriteStream(
-  path.join(__dirname, 'access.log'),
-  { flags: 'a' }
+  path.join(__dirname, "access.log"),
+  { flags: "a" }
 );
-app.use(morgan('combined', { stream: accessLogStream }));
+app.use(morgan("combined", { stream: accessLogStream }));
 // and log to the console
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 // statically serve the frontend
-_;
+app.use(express.static("public"));
 
 // - declare routes -
 
 // read all file names
-app._('_', (req, res, next) => {
-  fs._(FILES_DIR, (err, list) => {
-    if (_) {
+app.get("/files", (req, res, next) => {
+  fs.readdir(FILES_DIR, (err, list) => {
+    if (!list) {
       res.status(404).end();
-      _;
+      return;
     }
     if (err) {
       // https://expressjs.com/en/guide/error-handling.html
       next(err);
       return;
     }
-
     res.json(list);
   });
 });
 
 // read a file
-app._('_', (req, res, next) => {
+app.get("/files/:name", (req, res, next) => {
   const fileName = req.params.name;
-  fs._(`${FILES_DIR}/${fileName}`, _, (err, fileText) => {
-    if (_) {
-      _;
+  fs.readFile(`${FILES_DIR}/${fileName}`, "utf-8", (err, fileText) => {
+    if (!fileName) {
+      res.status(404).send("Not Found");
       return;
     }
-    if (_) {
-      _;
-      _;
+    if (err) {
+      res.status(400).send("Bad request");
+      return;
     }
 
     const responseData = {
@@ -72,45 +72,50 @@ app._('_', (req, res, next) => {
 });
 
 // write a file
-app._('_', (req, res, next) => {
-  const fileName = _;
-  const fileText = _;
-  fs._(`${FILES_DIR}/${fileName}`, _, err => {
-    if (_) {
-      _;
-      _;
+app.post("/files/:name", (req, res, next) => {
+  const fileName = req.params.name;
+  const fileText = req.body.text;
+  fs.writeFile(`${FILES_DIR}/${fileName}`, fileText, (err) => {
+    if (err) {
+      res.status(400).send(err);
+      return;
     }
 
     // https://stackoverflow.com/questions/33214717/why-post-redirects-to-get-and-put-redirects-to-put
-    res.redirect(303, '/files');
+    res.redirect(303, "/files");
   });
 });
 
-// delete a file
-app._('_', (req, res, next) => {
-  const fileName = _;
-  fs._(`${FILES_DIR}/${fileName}`, err => {
-    if (err && err.code === 'ENOENT') {
-      _;
-      _;
+// // delete a file
+app.delete("/files/:name", (req, res, next) => {
+  const fileName = req.params.name;
+  console.log(fileName);
+  fs.unlink(`${FILES_DIR}/${fileName}`, (err) => {
+    if (err && err.code === "ENOENT") {
+      res.status(404).send("File not found");
+      return;
     }
-    if (_) {
-      _;
-      _;
+    if (!fileName) {
+      res.status(404).send("File not found");
+      return;
     }
 
-    res.redirect(303, '/files');
+    res.redirect(303, "/files");
   });
 });
 
-// - handle errors in the routes and middleware -
+// // - handle errors in the routes and middleware -
 
 // https://expressjs.com/en/guide/error-handling.html
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   console.error(err.stack);
   res.status(500).end();
 });
 
 // - open server -
 // try to exactly match the message logged by demo.min.js
-_;
+app.listen(config.PORT, () => {
+  console.log(
+    `simple editor: running app on port:${config.PORT} (${config.MODE} mode)`
+  );
+});
